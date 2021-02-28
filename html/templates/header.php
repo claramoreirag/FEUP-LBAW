ee4a33c321882f496138b0d6efee13d70ac1a536
Mariana Ramos <7marianaramos@gmail.com>
2021-02-28 17:54:05 +0000

    organization and header

---
+++ html/templates/header.php
@@ -0,0 +1,35 @@
+<?php?>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="../style/global.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</head>
+    <body>
+        <header>
+           <div>
+                <div class="row navbar navbar-expand-lg navbar-light fixed-top" id="nav">
+                    <div class="col-1"></div>
+                    <div class="col-10">
+                        <div class="row justify-content-between align-items-center">
+                            <div class="col-4" id="title">GreenNews</div>
+                            <div class="col-2">
+                                <a class="log" href="">Sign In</a>
+                                <a class="log" href=""> | Log In</a>
+                            </div> 
+                        </div>
+                    </div>
+                    <div class="col-1"></div>
+                </div>
+            </div>
+        <header>
+
+        
+    </body>
+
+<php?>
---
+++ html/templates/post.php
@@ -0,0 +1,35 @@
+<?php?>
+
+    <body>
+
+    <div class="container" id="post">
+        <div class="row">
+            <div class="col-1"></div>
+            <div class="col-10">
+                <div class="row justify-content-between post-header">
+                    <div class="col-7">
+                        <div class="row">
+                            <div class="col-2" id="profile_pic">Pic</div>
+                            <div class="col-10" id="title">Title</div>
+                        </div>
+                    </div>
+                    <div class="col-2" id="tags">Tag</div>
+                </div>
+
+                <div class="row post-content">
+                    
+                </div>
+
+                <div class="row post-interactions">
+                    
+                </div>
+
+
+            </div>
+            <div class="col-1"></div>
+        </div>
+    </div>
+
+    </body>
+
+<php?>
