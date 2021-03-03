72c276ef2f4ce84347c3e321847ff049274f6308
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2021-03-03 16:39:07 +0000

    progress in authUserFeed

--- html/templates/authUserFeed.php
+++ html/templates/authUserFeed.php
@@ -7,58 +7,43 @@
 </head>
 
 <body>
-<div class=" container text-center">
-    <div class="row ">
-
-
-        <div class="col-md-7 ">
-
-            <div class="row justify-content-between">
-                <header class="col-md-3 home">
-                    <h1>Home</h1>
-                </header>
-                <div class="col-md-3">
-                    <a class="btn btn-primary " id="new-post-btn" href=""><i class="fas fa-plus"></i> New Post</a>
+<div class=" container text-center homepage">
+    
+    
+    
+    
+    <div class="row">
+    
+            <div class="col-md-9  posts">
+                <div class="row">
+                    <div class="input-group rounded search-container">
+                        <input type="search" class="form-control rounded searchbar" style=" border-radius: 2rem;color: var(--text-color); background-color: var(--background-color);" id="searchbar" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                        <span class="input-group-text border-0 search-icon">
+                            <i class="fas fa-search"></i>
+                        </span>
+                    </div>
+    
                 </div>
-            </div>
-            <div>
-                <?php drawPost(0) ?>
-                <?php drawPost(0) ?>
-                <?php drawPost(0) ?>
-            </div>
-
-        </div>
-
-
-
-
-
-        <div class="col-md-5">
-
-
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown button
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
+    
+                <div>
+                    <?php drawPost(0) ?>
+                    <?php drawPost(0) ?>
+                    <?php drawPost(0) ?>
                 </div>
+    
             </div>
-
-
-            <div class="row">
-                <div class="input-group rounded search-container">
-                    <input type="search" class="form-control rounded searchbar" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-                    <span class="input-group-text border-0" id="search-addon">
-                        <i class="fas fa-search"></i>
-                    </span>
-                </div>
-
+    
+            <div class="col-md-1"></div>
+        <div class="col-md-2 filters-bar  ">
+        <div class="filters" style="position: fixed;"> <header>Search filters</header>
+               
+               
             </div>
         </div>
 
 
-        </body>
-        <php?>
+    </div>
+    </div>
+    
+    </body>
+    <php?>
