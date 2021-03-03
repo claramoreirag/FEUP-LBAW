8ea2edd64676c036fd2ecc169d00341fc1a91d6f
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2021-03-03 03:08:06 +0000

    homepage almost done

--- html/templates/header.php
+++ html/templates/header.php
@@ -9,11 +9,12 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
 </head>
     <body>
         <header>
-           <div>
-                <div class="row navbar navbar-expand-lg navbar-light fixed-top" id="nav">
+           <div class="container">
+                <div class="row navbar navbar-expand-lg navbar-light fixed-top " id="nav">
                     <div class="col-1"></div>
                     <div class="col-10">
                         <div class="row justify-content-between align-items-center">
