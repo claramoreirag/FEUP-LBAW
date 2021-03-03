fb714492966e7dbd9c72d40d374f538fa0fbbd41
Leonor Marques Gomes <up201806567@fe.up.pt>
2021-03-03 16:42:10 +0000

    Start profile page

--- html/pages/myProfilePage.php
+++ html/pages/myProfilePage.php
@@ -1,5 +1,7 @@
 <?php
   include_once('../templates/header.php');
 
+  include_once('../templates/ownProfile.php');
+
   
 ?>
---
+++ html/templates/ownProfile.php
@@ -0,0 +1,61 @@
+<head>
+  <link rel="stylesheet" href="../style/ownProfile.css">
+</head>
+
+<body>
+  <div class="row">
+    <div class="col-1"></div>
+    <div class="col-8" id="myPosts">
+      <div class="row" id="postOptions">
+        <div class="col-4"><a href="" class="btn btn-primary">My Posts</a></div>
+        <div class="col-4"><a href="" class="btn btn-primary">Saved News</a></div>
+        <div class="col-4"><a href="" class="btn btn-primary">Upvotes</a></div>
+      </div>
+    </div>
+    <div class="col-2" id="myInfo">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 d-flex justify-content-center" id="profileImg">
+            <img src="../images/profilepic1.jpg" alt="profile picture">
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6 d-flex justify-content-center">
+          <h3 id="name">
+            Name
+          </h3>
+        </div>
+        <div class="col-3"></div>
+      </div>
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6 d-flex justify-content-center">
+          <h4 id="username">
+            @username
+          </h4>
+        </div>
+        <div class="col-3"></div>
+      </div>
+      <div class="row">
+        <div class="col-12" id="infoNumbers">
+          <ul>
+            <li> <span class="fas fa-user"></span> 12 followers </li>
+            <li> <span class="fas fa-user"></span> 11 following </li>
+            <li> <span class="fas fa-newspaper"></span> 3 posts </li>
+            <li> <span class="fas fa-arrow-up"></span> 256 upvotes </li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-2"></div>
+        <div class="col-9" id="editProfile">
+          <span class="fas fa-edit"></span> Edit profile
+        </div>
+        <div class="col-1"></div>
+      </div>
+    </div>
+    <div class="col-1"></div>
+  </div>
+</body>
