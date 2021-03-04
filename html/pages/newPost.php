84090783f9eb1149702e885fe0b0ddf666b034a8
Mariana Ramos <7marianaramos@gmail.com>
2021-03-04 16:59:27 +0000

    new post page

--- html/pages/newPost.php
+++ html/pages/newPost.php
@@ -34,10 +34,22 @@
                             </select>
                         </div>
                     </div>
+                    <div class="container mt-4 mb-4">
+  <div class="row justify-content-md-center">
+    <div class="col-md-12 col-lg-8">
+      <h1 class="h2 mb-4">Submit issue</h1>
+      <label>Describe the issue in detail</label>
+      <div class="form-group">
+         <textarea id="editor"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</div>
                     <div class="form-group">
-                                <label for="inputNewsBody">Body</label>
-                                <input type="body" class="form-control" id="inputNewsBody" placeholder="Body">
-                            </div>
+                            <label for="inputNewsBody">Body</label>
+                            <input type="body" class="form-control" id="inputNewsBody" placeholder="Body">
+                    </div>
                     <div class="form-group form-images">
                         <label class="form-label" for="customFile">Default file input example</label>
                         <input type="file" class="form-control" id="customFile" />
