506e964efd40d3b73cffe6218b1611ec61d21159
Flávia Carvalho Gavinha Pereira Carvalhido <up201806857@fe.up.pt>
2021-03-04 17:44:33 +0000

    Log In and Sign Up Done

---
+++ html/templates/adminHeader.php
@@ -0,0 +1,34 @@
+<?php?>
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>GreeNews</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="../style/global.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div>
+            <div class="row navbar navbar-expand-lg navbar-light fixed-top" id="nav">
+                <div class="col-1"></div>
+                <div class="col-10">
+                    <div class="row justify-content-between align-items-center">
+                        <div class="col-4" id="title">GreeNews</div>
+                        <div class="col-2">
+                            <p class="admin_head">Admin |</p>
+                            <a class="log" href=""> Log Out</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-1"></div>
+            </div>
+        </div>
+    </header>
+    <?php?>
