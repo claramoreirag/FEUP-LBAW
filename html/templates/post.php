f09465e17c63685e0bd67b9689f2a0f7d3b879cd
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2021-03-04 17:37:59 +0000

    added about us page

--- html/templates/post.php
+++ html/templates/post.php
@@ -2,6 +2,8 @@
 {
 
 ?>
+
+
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <link rel="stylesheet" href="../style/postPreview.css"">
 
@@ -12,8 +14,8 @@
             <div class="row justify-content-between post-header">
                 <div class="col-7">
                     <div class="row">
-                        <div class="col-2" id="profile-pic"><img class="" src=".././img/actors.png" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
-                        <div class="col-10" id="title">Title</div>
+                        <div class="col-1" id="profile-pic"><img class="" src=".././img/actors.png" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
+                        <div class="col-11" id="title">Title</div>
                     </div>
                 </div>
                 <div class="col-2" id="tags">Tag</div>
@@ -21,7 +23,8 @@
 
             <div class="row justify-content-between post-content ">
                 <div class="col-8 ">
-                    <p>jhhhhhhhhhhhhh <br> gggggggggfjfjfjfjffjfjfccccccccccjf<br>ccccccccccccccccccc</p>
+                    <p>Mauris consectetur vehicula facilisis. Etiam aliquet accumsan libero, et aliquam tortor tincidunt eget. Maecenas ut feugiat velit. Aenean fringilla fermentum eros, a efficitur nibh. Aenean vulputate erat et nisi tristique iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ligula purus, iaculis quis ipsum elementum, ullamcorper condimentum lacus.
+                    <br>In id enim efficitur dolor ornare tempus. Maecenas condimentum molestie volutpat. Phasellus sagittis erat metus, eu gravida metus lacinia ac.</p>
                 </div>
                 <div class="col-3 post-pic">
                     <img class="" src="" alt="template pic" width="150" height="150">
@@ -29,13 +32,15 @@
             </div>
 
             <div class="row post-interactions justify-content-between">
-               
-                <div class="col-1 share"><i class="fas fa-share-alt"></i></div>
-               
-                <div class="col-3" style="margin-right:0rem; padding-right:0rem ;margin-top:0.5rem">
+                <div class="col-1 actions"></i>
+                   
+                        <div class=" share action"><i class="fas fa-share-alt"></i></div>
+                       
+                </div>
+                <div class="col-2" style="margin-right:0rem; padding-right:0rem ;margin-top:0.5rem">
                     <div class="row justify-content-end votes">
-                        <div class="col-6"><i class="fas fa-arrow-up"></i> x </div>
-                        <div class="col-6"><i class="fas fa-arrow-down"></i> y </div>
+                        <div class="col-6 upvote"><i class="fas fa-arrow-up"></i> 10 </div>
+                        <div class="col-6 downvote"><i class="fas fa-arrow-down"></i> 3 </div>
                          
                     </div>
                 </div>
@@ -46,5 +51,73 @@
 
     </div>
     </div>
+
+
+
+    
+
+<?php } ?>
+
+
+
+
+<?php function drawAuthPost($postID)
+{
+
+?>
+
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <link rel="stylesheet" href="../style/postPreview.css"">
+
+    <div class=" container-fluid" id="post">
+    <div class="row">
+
+        <div class="col-12">
+            <div class="row justify-content-between post-header">
+                <div class="col-7">
+                    <div class="row">
+                        <div class="col-1" id="profile-pic"><img class="" src=".././img/actors.png" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
+                        <div class="col-11" id="title">Title</div>
+                    </div>
+                </div>
+                <div class="col-2" id="tags">Tag</div>
+            </div>
+
+            <div class="row justify-content-between post-content ">
+                <div class="col-8 ">
+                    <p>Mauris consectetur vehicula facilisis. Etiam aliquet accumsan libero, et aliquam tortor tincidunt eget. Maecenas ut feugiat velit. Aenean fringilla fermentum eros, a efficitur nibh. Aenean vulputate erat et nisi tristique iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ligula purus, iaculis quis ipsum elementum, ullamcorper condimentum lacus.
+                    <br>In id enim efficitur dolor ornare tempus. Maecenas condimentum molestie volutpat. Phasellus sagittis erat metus, eu gravida metus lacinia ac.</p>
+                </div>
+                <div class="col-3 post-pic">
+                    <img class="" src="" alt="template pic" width="150" height="150">
+                </div>
+            </div>
+
+            <div class="row post-interactions justify-content-between">
+                <div class="col-2 actions"></i>
+                    <div class="row ">
+                        <div class="col-4 share action"><i class="fas fa-share-alt"></i></div>
+                        <div class="col-4 save action"><i class="fas fa-bookmark"></i></div>
+                        <div class="col-4 report action"><i class="fas fa-exclamation-circle"></i></div>
+                    </div>
+                </div>
+                <div class="col-2" style="margin-right:0rem; padding-right:0rem ;margin-top:0.5rem">
+                    <div class="row justify-content-end votes">
+                        <div class="col-6 upvote"><i class="fas fa-arrow-up"></i> 10 </div>
+                        <div class="col-6 downvote"><i class="fas fa-arrow-down"></i> 3 </div>
+                         
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+    </div>
+
+
+
+    
 
 <?php } ?>
