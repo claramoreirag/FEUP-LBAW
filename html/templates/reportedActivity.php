260ca7df4cdab0b70c09a622e81193eea0d798ce
Flávia Carvalho Gavinha Pereira Carvalhido <up201806857@fe.up.pt>
2021-03-07 20:24:57 +0000

    Finish Admin Dashboard Responsiveness

--- html/templates/reportedActivity.php
+++ html/templates/reportedActivity.php
@@ -2,34 +2,38 @@
 {
 ?>
 
-    <div class=" row">
+    <div class="row">
 
-        <div class="col-10">
+        <div class="col-12 col-md-10">
 
             <div class="row justify-content-end">
-                <div class="col-3">
+                <div class="col">
                     <p class="type_date">Post, 02/03/2021 12:57</p>
                 </div>
             </div>
 
 
-            <div class=" container-fluid" id="post">
+            <div class=" container-fluid d-flex" id="post">
                 <div class="row">
 
-                    <div class="col-12">
+                    <div class="col-12 justify-content-sm-end">
 
                         <div class="row justify-content-between post-header">
                             <div class="col-7">
                                 <div class="row">
                                     <div class="col-1" id="profile-pic"><img class="" src="../images/profilepic1.jpg" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
-                                    <div class="col-11" id="title">The Earth is Flat. Change my mind.</div>
+                                    <div class="col" id="title">
+                                        <p>The Earth is Flat. Change my mind.</p>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="col-2 tags">3 x reported</div>
+                            <div class="col-sm-5 col-md-4 tag-holder ">
+                                <p class="btn btn-primary tags">3 x reported</p>
+                            </div>
                         </div>
 
                         <div class="row justify-content-between post-content ">
-                            <div class="col-8 ">
+                            <div class="col-lg-8 col-md-9 col-sm-12">
                                 <p>Mauris consectetur vehicula facilisis. Etiam aliquet accumsan libero, et aliquam tortor tincidunt eget. Maecenas ut feugiat velit. Aenean fringilla fermentum eros, a efficitur nibh. Aenean vulputate erat et nisi tristique iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ligula purus, iaculis quis ipsum elementum, ullamcorper condimentum lacus.
                                     <br>In id enim efficitur dolor ornare tempus. Maecenas condimentum molestie volutpat. Phasellus sagittis erat metus, eu gravida metus lacinia ac.
                                 </p>
@@ -46,8 +50,7 @@
 
         </div>
 
-        <div class="col-2 actions d-none d-sm-flex flex-column">
-            <!-- TODO: mantenho a descrição dos botões ou ficam só os icones com botões pequeninos -->
+        <div class="col-2 actions d-none d-md-flex flex-column">
 
             <div class="row flex-grow-1"></div>
 
@@ -72,6 +75,26 @@
         </div>
     </div>
 
+    <div class="row actions d-md-none">
+
+        <div class="col-6 col-sm-3 justify-content-center">
+            <button type="button" class="btn btn-light"><i class="far fa-trash-alt"></i> Delete</button>
+        </div>
+
+        <div class="col-6 col-sm-3 justify-content-center">
+            <button type="button" class="btn btn-light"><i class="fas fa-user-clock"></i> Suspend User</button>
+        </div>
+
+        <div class="col-6 col-sm-3 justify-content-center">
+            <button type="button" class="btn btn-light"><i class="fas fa-user-slash"></i> Ban User</button>
+        </div>
+
+        <div class="col-6 col-sm-3 justify-content-center">
+            <button type="button" class="btn btn-light"><i class="far fa-check-circle"></i> Dismiss</button>
+        </div>
+
+    </div>
+
 
 <?php } ?>
 
@@ -81,32 +104,34 @@
 
     <div class="row">
 
-        <div class="col-10">
+        <div class="col-12 col-md-10">
 
             <div class="row justify-content-end">
-                <div class="col-3">
+                <div class="col">
                     <p class="type_date">Comment, 02/03/2021 12:57</p>
                 </div>
             </div>
 
 
-            <div class=" container-fluid" id="post">
+            <div class="container-fluid d-flex" id="post">
                 <div class="row">
 
-                    <div class="col-12">
+                    <div class="col-12 justify-content-sm-end">
 
                         <div class="row justify-content-between post-header">
                             <div class="col-7">
                                 <div class="row">
                                     <div class="col-1" id="profile-pic"><img class="" src="../images/profilepic1.jpg" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
-                                    <div class="col-11" id="title">@diesel_is_cool</div>
+                                    <div class="col-sm-5 col-md-4" id="title">@diesel_is_cool</div>
                                 </div>
                             </div>
-                            <div class="col-2 tags">5 x reported</div>
+                            <div class="col-sm-5 col-md-4 tag-holder ">
+                                <p class="btn btn-primary tags">5 x reported</p>
+                            </div>
                         </div>
 
                         <div class="row justify-content-between comment-content ">
-                            <div class="col">
+                            <div class="col-lg-8 col-md-9 col-sm-12">
                                 <p>Mauris consectetur vehicula facilisis. Etiam aliquet accumsan libero, et aliquam tortor tincidunt eget. Maecenas ut feugiat velit. Aenean fringilla fermentum eros, a efficitur nibh. Aenean vulputate erat et nisi tristique iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ligula purus, iaculis quis ipsum elementum, ullamcorper condimentum lacus.
                                     <br>In id enim efficitur dolor ornare tempus. Maecenas condimentum molestie volutpat. Phasellus sagittis erat metus, eu gravida metus lacinia ac.
                                 </p>
@@ -120,8 +145,7 @@
 
         </div>
 
-        <div class="col-2 actions d-none d-sm-flex flex-column">
-            <!-- TODO: mantenho a descrição dos botões ou ficam só os icones com botões pequeninos -->
+        <div class="col-2 actions d-none d-md-flex flex-column">
 
             <div class="row flex-grow-1"></div>
 
@@ -144,6 +168,26 @@
             <div class="row flex-grow-1"></div>
 
         </div>
+    </div>
+
+    <div class="row actions d-md-none">
+
+        <div class="col-6 col-sm-3 justify-content-center">
+            <button type="button" class="btn btn-light"><i class="far fa-trash-alt"></i> Delete</button>
+        </div>
+
+        <div class="col-6 col-sm-3 justify-content-center">
+            <button type="button" class="btn btn-light"><i class="fas fa-user-clock"></i> Suspend User</button>
+        </div>
+
+        <div class="col-6 col-sm-3 justify-content-center">
+            <button type="button" class="btn btn-light"><i class="fas fa-user-slash"></i> Ban User</button>
+        </div>
+
+        <div class="col-6 col-sm-3 justify-content-center">
+            <button type="button" class="btn btn-light"><i class="far fa-check-circle"></i> Dismiss</button>
+        </div>
+
     </div>
 
 
