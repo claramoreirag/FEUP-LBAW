9430d61c6bba0cdf571b00631c8d41b40fc09fde
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2021-03-08 21:40:23 +0000

    posts with content

--- html/templates/post.php
+++ html/templates/post.php
@@ -7,30 +7,29 @@
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
     <link rel="stylesheet" href="../style/postPreview.css"">
 
-    <div class=" container-fluid d-flex" id="post">
+    <div class=" container-fluid " id="post">
     <div class="row">
 
-        <div class="col-12 justify-content-sm-end ">
+        <div class="col-12 justify-content-end ">
             <div class="row justify-content-between post-header">
-                <div class="col-7">
-                    <div class="row">
-                        <div class="col-1" id="profile-pic"><img class="" src=".././img/actors.png" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
-                        <div class="col-md-5 col-sm-6" id="title">Title</div>
+                <div class="col-xs-12 col-sm-8">
+                    <div class="row ">
+                        <div class=" col-sm-1" id="profile-pic"><img class="" src="../images/profilepic1.jpg" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
+                        <div class="col-10 " id="title">Low-level jets create winds of change for turbines</div>
                     </div>
                 </div>
-                <div class="col-sm-5 col-md-4 tag-holder ">
+                <div class="col-sm-3 col-md-3 tag-holder ">
                     <button class="btn btn-primary tags">Tag</button>
                 </div>
             </div>
 
             <div class="row justify-content-between post-content ">
-                <div class="col-lg-8 col-md-9 col-sm-12">
-                    <p class="post-text">Mauris consectetur vehicula facilisis. Etiam aliquet accumsan libero, et aliquam tortor tincidunt eget. Maecenas ut feugiat velit. Aenean fringilla fermentum eros, a efficitur nibh. Aenean vulputate erat et nisi tristique iaculis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut ligula purus, iaculis quis ipsum elementum, ullamcorper condimentum lacus.
-                    <br>In id enim efficitur dolor ornare tempus. Maecenas condimentum molestie volutpat. Phasellus sagittis erat metus, eu gravida metus lacinia ac.</p>
+                <div class="col-lg-8 col-md-9 col-sm-12 ">
+                    <p class="post-text ">As one of the leading sources of clean and renewable energy, global wind power capacity has increased more than fivefold over the past decade, leading to larger turbines and pushing wind technology to its limits.</p>
                     <a href="../pages/fullPostPage.php" class="read-more">Read More</a>
                 </div>
                 <div class="col-3 post-pic">
-                    <img class="" src="" alt="template pic" width="150" height="150">
+                    <img class="" src="../images/news1.jpg" alt="template pic" width="170" >
                 </div>
             </div>
 
@@ -77,9 +76,9 @@
 
     <div class="col-12 justify-content-sm-end ">
             <div class="row justify-content-between post-header">
-                <div class="col-7">
+                <div class="col-7 col-sm-12">
                     <div class="row">
-                        <div class="col-1" id="profile-pic"><img class="" src=".././img/actors.png" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
+                        <div class="col-1 col-sm-5" id="profile-pic"><img class="" src=".././img/actors.png" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
                         <div class="col-md-5 col-sm-6" id="title">Title</div>
                     </div>
                 </div>
