a9227526a1848ca90bd870d0a362606ed1ece678
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2021-03-11 00:50:02 +0000

    refactoring homepage

--- html/templates/homepage.php
+++ html/templates/homepage.php
@@ -1,22 +1,63 @@
-
-
 <?php include_once('../templates/post.php');
 ?>
 
-    <link rel="stylesheet" href="../style/homepage.css"">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="../style/bootstrap.css"">
+    <script src=" https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+</script>
 
 
 
 
 <div class=" container homepage">
 
-
+    
 
 
     <div class="row">
+    <div class="col-md-2 filters-bar  ">
+            <div class="filters" style="position: fixed;">
+                <h3 class="filters-title form-label ">Search filters</h3>
+                <form>
+                    <legend class="form-label label ">Tags</legend>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="tag1">
+                        <label class="form-check-label" for="flexCheckDefault">Energy</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="tag2" checked>
+                        <label class="form-check-label" for="flexCheckChecked">Clean Water</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="tag3">
+                        <label class="form-check-label" for="flexCheckDefault">Animal Cruelty</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="tag4" checked>
+                        <label class="form-check-label" for="flexCheckChecked">Research</label>
+                    </div>
 
-    <div class="col-md-9  posts">
+
+
+                    <div class="form-group">
+                        <legend class="form-label label order-by">Order By</legend>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="feedType" id="top-news">
+                            <label class="form-check-label" for="flexRadioDefault1">Top News</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="feedType" id="newest" checked>
+                            <label class="form-check-label" for="flexRadioDefault2"> Newest</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary form-submit">Submit</button>
+                </form>
+
+            </div>
+        </div>
+    <div class="col-md-1"></div>
+        <div class="col-md-9  posts">
             <div class="row">
                 <div class="input-group rounded search-container">
                     <input type="search" class="form-control rounded searchbar" style=" border-radius: 2rem;color: var(--text-color); background-color: var(--background-color);" id="searchbar" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
@@ -28,7 +69,7 @@
             </div>
 
             <form>
-                <div class="row d-md-none">
+                <div class="row ">
                     <header class="filters-title form-label">Search filters</header>
                     <div class="col-sm-4 filter">
                         <legend class="form-label label">Tags</legend>
@@ -49,7 +90,7 @@
                             <label class="form-check-label" for="flexCheckChecked">Research</label>
                         </div>
                     </div>
-                  
+
                     <div class="col-sm-4 " style="vertical-align:top;">
                         <div class="form-group">
                             <legend class="form-label label">Order By</legend>
@@ -65,66 +106,25 @@
                         </div>
                     </div>
                     <div class="col-sm-5">
-                    <button type="submit" class="btn btn-primary form-submit">Submit</button>
+                        <button type="submit" class="btn btn-primary form-submit">Submit</button>
                     </div>
-          
+
                 </div>
             </form>
-        
+
             <div class="row posts-container">
+                <?php drawP(0) ?>
+                <?php drawP(0) ?>
                 <?php drawPost(0) ?>
-                <?php drawPost(0) ?>
-                <?php drawPost(0) ?>
-            </div>
-    </div>
-
-        <div class="col-md-1"></div>
-        <div class="col-md-2 filters-bar  ">
-            <div class="filters" style="position: fixed;">
-                <header class="filters-title form-label ">Search filters</header>
-                <form>
-                <legend class="form-label label ">Tags</legend>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="tag1">
-                        <label class="form-check-label" for="flexCheckDefault">Energy</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="tag2" checked>
-                        <label class="form-check-label" for="flexCheckChecked">Clean Water</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="tag3">
-                        <label class="form-check-label" for="flexCheckDefault">Animal Cruelty</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="tag4" checked>
-                        <label class="form-check-label" for="flexCheckChecked">Research</label>
-                    </div>
-                   
-                   
-                   
-                    <div class="form-group">
-                        <legend class="form-label label order-by">Order By</legend>
-                       
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="feedType" id="top-news">
-                            <label class="form-check-label" for="flexRadioDefault1">Top News</label>
-                        </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="feedType" id="newest" checked>
-                            <label class="form-check-label" for="flexRadioDefault2"> Newest</label>
-                        </div>
-                    </div>
-                    
-                    <button type="submit" class="btn btn-primary form-submit">Submit</button>
-                </form>
-
             </div>
         </div>
 
+        
+        
+
 
     </div>
-    </div>
+</div>
 
-    </body>
-        <php?>
+</body>
+<php?>
--- html/templates/post.php
+++ html/templates/post.php
@@ -1,3 +1,64 @@
+<?php function drawP($postID)
+{
+
+?>
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <link rel="stylesheet" href="../style/bootstrap.css">
+    <div class="card mb-3">
+        <div class="card-body ">
+            <div class="row mb-2 justify-content-end">
+                <div class="col-sm-3 col-md-3  " style="text-align:end">
+                    <span class="badge badge-primary tag p-1">Tag</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-1 " style="margin-right:1rem;">
+                    <img class="" src="../images/profilepic1.jpg" alt="profile pic" width="40" height="40" style="border-radius: 50%;">
+                </div>
+                <div class="col-10">
+                    <h4 class="card-title text-primary">Low-level jets create winds of change for turbines</h4>
+                </div>
+
+            </div>
+
+            <h6 class="card-subtitle mt-2 mb-2 text-muted">By @johndoe on 20/01/2021 20:40</h6>
+            <div class="card-text">
+                <div class="row">
+
+                    <div class="col-lg-8 col-md-9 col-sm-12 mb-2">
+                        <p class="mb-0">As one of the leading sources of clean and renewable energy, global wind power capacity has increased more than fivefold over the past decade, leading to larger turbines and pushing wind technology to its limits.</p>
+                        <a href="../pages/fullPostPage.php" class="read-more">Read More</a>
+                        <br>
+                    </div>
+                    <div class="col-md-4 col-xs-6 post-pic">
+                        <img class="img-fluid" src="../images/news1.jpg" alt="template pic">
+                    </div>
+                </div>
+              
+            </div>
+
+            <div class="row">
+                <div class="col-lg-2 col-md-3 col-sm-4 mt-2">
+                    <div class="row justify-content-end votes">
+                        <div class="col-6 upvote"><i class="fas fa-arrow-up"></i> 10 </div>
+                        <div class="col-6 downvote"><i class="fas fa-arrow-down"></i> 3 </div>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+<?php } ?>
+
+
+
+
+
 <?php function drawPost($postID)
 {
 
@@ -5,12 +66,13 @@
 
 
     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-    <link rel="stylesheet" href="../style/bootstrap.css"">
+    <link rel="stylesheet" href="../style/bootstrap.css">
+    <link rel="stylesheet" href="../style/postPreview.css"">
 
-    <div class=" container-fluid " id="post">
+    <div class=" container-fluid " id=" post">
     <div class="row">
+        <div class="card border-primary mb-3">
 
-        <div class="col-12 justify-content-end ">
             <div class="row justify-content-between post-header">
                 <div class="col-xs-12 col-sm-8">
                     <div class="row ">
@@ -19,7 +81,8 @@
                     </div>
                 </div>
                 <div class="col-sm-3 col-md-3 tag-holder ">
-                    <button class="btn btn-primary tags">Tag</button>
+                    <span class="badge badge-primary tag">Tag</span>
+
                 </div>
             </div>
 
@@ -30,21 +93,21 @@
                     <br>
                 </div>
                 <div class="col-3 post-pic">
-                    <img class="" src="../images/news1.jpg" alt="template pic" width="170" >
+                    <img class="" src="../images/news1.jpg" alt="template pic" width="170">
                 </div>
             </div>
 
             <div class="row post-interactions justify-content-between">
                 <div class="col-sm-1 col-sm-2 actions"></i>
-                   
-                        <div class=" share action"><i class="fas fa-share-alt"></i></div>
-                       
+
+                    <!-- <div class=" share action"><i class="fas fa-share-alt"></i></div> -->
+
                 </div>
-                <div class="col-lg-2 col-md-3 col-sm-4" >
+                <div class="col-lg-2 col-md-3 col-sm-4">
                     <div class="row justify-content-end votes">
                         <div class="col-6 upvote"><i class="fas fa-arrow-up"></i> 10 </div>
                         <div class="col-6 downvote"><i class="fas fa-arrow-down"></i> 3 </div>
-                         
+
                     </div>
                 </div>
             </div>
@@ -57,7 +120,7 @@
 
 
 
-    
+
 
 <?php } ?>
 
@@ -75,7 +138,7 @@
     <div class=" container-fluid d-flex" id="post">
     <div class="row">
 
-    <div class="col-12 justify-content-end ">
+        <div class="col-12 justify-content-end ">
             <div class="row justify-content-between post-header">
                 <div class="col-xs-12 col-sm-8">
                     <div class="row ">
@@ -94,7 +157,7 @@
                     <a href="../pages/fullPostPage.php" class="read-more">Read More</a>
                 </div>
                 <div class="col-3 post-pic">
-                    <img class="" src="../images/news1.jpg" alt="template pic" width="170" >
+                    <img class="" src="../images/news1.jpg" alt="template pic" width="170">
                 </div>
             </div>
             <div class="row post-interactions justify-content-between">
@@ -105,11 +168,11 @@
                         <div class="col-4 report action"><i class="fas fa-exclamation-circle"></i></div>
                     </div>
                 </div>
-                <div class="col-lg-2 col-md-3 col-sm-4" >
+                <div class="col-lg-2 col-md-3 col-sm-4">
                     <div class="row justify-content-end votes">
                         <div class="col-6 upvote"><i class="fas fa-arrow-up"></i> 10 </div>
                         <div class="col-6 downvote"><i class="fas fa-arrow-down"></i> 3 </div>
-                         
+
                     </div>
                 </div>
             </div>
@@ -122,7 +185,7 @@
 
 
 
-    
+
 
 <?php } ?>
 
@@ -141,7 +204,7 @@
     <div class=" container-fluid" id="post">
     <div class="row">
 
-    <div class="col-12 justify-content-end ">
+        <div class="col-12 justify-content-end ">
             <div class="row justify-content-between post-header">
                 <div class="col-xs-12 col-sm-8">
                     <div class="row ">
@@ -161,7 +224,7 @@
                     <a href="../pages/fullPostPage.php" class="read-more">Read More</a>
                 </div>
                 <div class="col-3 post-pic">
-                    <img class="" src="../images/news1.jpg" alt="template pic" width="170" >
+                    <img class="" src="../images/news1.jpg" alt="template pic" width="170">
                 </div>
             </div>
             <div class="row post-interactions justify-content-between">
@@ -169,14 +232,14 @@
                     <div class="row ">
                         <div class="col-6 share action"><i class="fas fa-share-alt"></i></div>
                         <div class="col-6 save action"><span class="fas fa-edit"></div>
-                    
+
                     </div>
                 </div>
                 <div class="col-2" style="margin-right:0rem; padding-right:0rem ;margin-top:0.5rem">
                     <div class="row justify-content-end votes">
                         <div class="col-6 upvote"><i class="fas fa-arrow-up"></i> 10 </div>
                         <div class="col-6 downvote"><i class="fas fa-arrow-down"></i> 3 </div>
-                         
+
                     </div>
                 </div>
             </div>
@@ -189,6 +252,6 @@
 
 
 
-    
+
 
 <?php } ?>
