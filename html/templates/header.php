cbb394fba2e34692a3640c86f8aa530a5a08cf94
Ana Clara Moreira Gadelho <up201806309@fe.up.pt>
2021-03-12 00:05:50 +0000

    merge

--- html/templates/header.php
+++ html/templates/header.php
@@ -78,6 +78,7 @@
     <?php function drawAuthHeader()
     {
     ?>
+
         <head>
             <meta charset='utf-8'>
             <meta http-equiv='X-UA-Compatible' content='IE=edge'>
@@ -90,7 +91,7 @@
 
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
-          
+
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -171,148 +172,150 @@
             </header>-->
 
 
-    <?php } ?>
+            <?php } ?>
 
 
 
 
-<?php function drawAboutUsHeader()
-{
-?>
+            <?php function drawAboutUsHeader()
+            {
+            ?>
 
-    <head>
-        <meta charset='utf-8'>
-        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-        <title>GreeNews</title>
-        <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel="stylesheet" href="../style/bootstrap.css">
-
-
-        <!-- Bootstrap CSS -->
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-        <!-- Latest compiled JavaScript -->
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                <head>
+                    <meta charset='utf-8'>
+                    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                    <title>GreeNews</title>
+                    <meta name='viewport' content='width=device-width, initial-scale=1'>
+                    <link rel="stylesheet" href="../style/bootstrap.css">
 
 
-        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+                    <!-- Bootstrap CSS -->
+
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+                    <!-- Latest compiled JavaScript -->
+                    <!-- Optional JavaScript -->
+                    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+                    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
+                    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+                    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
 
 
-    </head>
-
-    <body>
-        <header>
-            <div>
-                <nav class="row navbar navbar-expand-md navbar-dark bg-primary fixed-top">
-                    <div class="col-1"></div>
-                    <div class="col-10 ">
-                        <div class="row justify-content-between">
-                            <div class="col-4 ">
-                                <h4><a class="row text-white nav-title mt-2" id="title" href="../pages/homepage.php">GreeNews</a></h4>
-
-                            </div>
-                            <div class="col-md-1 px-0">
-                                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
-                            </div>
 
 
-                            <div class="col-md-4  collapse navbar-collapse justify-content-end" id="navbarNav">
+                </head>
 
-                                <a class="text-white log nav-item nav-link" href="../pages/login.php">Sign In</a>
-                                <span class="text-white d-none d-md-block separator"> | </span>
-                                <a class="text-white log nav-item nav-link " href="../pages/signup.php"> Log In</a>
+                <body>
+                    <header>
+                       
+                            <nav class="row navbar navbar-expand-md navbar-dark bg-primary fixed-top">
+                                <div class="col-1"></div>
+                                <div class="col-10 ">
+                                    <div class="row justify-content-between">
+                                        <div class="col-4 ">
+                                            <h4><a class="row text-white nav-title mt-2" id="title" href="../pages/homepage.php">GreeNews</a></h4>
 
-
-                            </div>
-
-                        </div>
-
-
-                    </div>
-                    <div class="col-1"></div>
-
-                </nav>
-        </header>
-
-
-
-    <?php } ?>
+                                        </div>
+                                        <div class="col-md-1 px-0">
+                                            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                                <span class="navbar-toggler-icon"></span>
+                                        </div>
 
 
-    <?php function drawAdminHeader()
-    {
-    ?>
-        <head>
-            <meta charset='utf-8'>
-            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-            <title>GreeNews</title>
-            <meta name='viewport' content='width=device-width, initial-scale=1'>
-            <link rel="stylesheet" href="../style/bootstrap.css">
+                                        <div class="col-md-4  collapse navbar-collapse justify-content-end" id="navbarNav">
+
+                                            <a class="text-white log nav-item nav-link" href="../pages/login.php">Sign In</a>
+                                            <span class="text-white d-none d-md-block separator"> | </span>
+                                            <a class="text-white log nav-item nav-link " href="../pages/signup.php"> Log In</a>
 
 
-            <!-- Bootstrap CSS -->
+                                        </div>
 
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-          
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                                    </div>
 
 
-            <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-            <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+                                </div>
+                                <div class="col-1"></div>
+
+                            </nav>
+
+                    </header>
 
 
-            <script src="https://cdn.tiny.cloud/1/up85hjw3nat4fck36d4b8sga07h0hs8y6j1nkiusyctbojab/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+                <?php } ?>
 
 
-        </head>
+                <?php function drawAdminHeader()
+                {
+                ?>
 
-        <body>
-            <header>
-                <nav class="row navbar navbar-expand-md navbar-dark bg-primary">
-                    <div class="col-1"></div>
-                    <div class="col-10 ">
-                        <div class="row justify-content-between">
-                            <div class="col-4 ">
-                                <h4><a class="row text-white nav-title" id="title" href="../pages/homepage.php">GreeNews</a></h4>
-                                <h6> <a class="row text-white nav-subtitle" id="aboutus" href="../pages/aboutUs.php">About Us</a></h6>
-                            </div>
-                            <div class="col-md-1 px-0">
-                                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
-                            </div>
+                    <head>
+                        <meta charset='utf-8'>
+                        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                        <title>GreeNews</title>
+                        <meta name='viewport' content='width=device-width, initial-scale=1'>
+                        <link rel="stylesheet" href="../style/bootstrap.css">
 
 
-                            <div class="col-md-4  collapse navbar-collapse justify-content-end" id="navbarNav">
+                        <!-- Bootstrap CSS -->
 
-                                <a class="log nav-item nav-link text-white" href="../pages/userManager.php"><i class="fas fa-users-cog"></i> </a>
-                                <a class="log nav-item nav-link text-white" href="../pages/adminDashboard.php"><i class="fas fa-exclamation-circle"></i> </a>
-                                <a class="log nav-item nav-link d-none d-md-block text-white" href="../pages/myProfilePage.php">Admin</a>
-                                <span class="d-none d-md-block separator text-white"> | </span>
-                                <a class="log nav-item nav-link text-white" href="../pages/homepage.php"> Sign Out</a>
-
-                            </div>
-
-                        </div>
+                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
 
-                    </div>
-                    <div class="col-1"></div>
+                        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-                </nav>
 
-                
+                        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+                        <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 
-    <?php } ?>
+
+                        <script src="https://cdn.tiny.cloud/1/up85hjw3nat4fck36d4b8sga07h0hs8y6j1nkiusyctbojab/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+
+                    </head>
+
+                    <body>
+                        <header>
+                            <nav class="row navbar navbar-expand-md navbar-dark bg-primary">
+                                <div class="col-1"></div>
+                                <div class="col-10 ">
+                                    <div class="row justify-content-between">
+                                        <div class="col-4 ">
+                                            <h4><a class="row text-white nav-title" id="title" href="../pages/homepage.php">GreeNews</a></h4>
+                                            <h6> <a class="row text-white nav-subtitle" id="aboutus" href="../pages/aboutUs.php">About Us</a></h6>
+                                        </div>
+                                        <div class="col-md-1 px-0">
+                                            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                                <span class="navbar-toggler-icon"></span>
+                                        </div>
+
+
+                                        <div class="col-md-4  collapse navbar-collapse justify-content-end" id="navbarNav">
+
+                                            <a class="log nav-item nav-link text-white" href="../pages/userManager.php"><i class="fas fa-users-cog"></i> </a>
+                                            <a class="log nav-item nav-link text-white" href="../pages/adminDashboard.php"><i class="fas fa-exclamation-circle"></i> </a>
+                                            <a class="log nav-item nav-link d-none d-md-block text-white" href="../pages/myProfilePage.php">Admin</a>
+                                            <span class="d-none d-md-block separator text-white"> | </span>
+                                            <a class="log nav-item nav-link text-white" href="../pages/homepage.php"> Sign Out</a>
+
+                                        </div>
+
+                                    </div>
+
+
+                                </div>
+                               
+
+                            </nav>
+
+
+
+                        <?php } ?>
