712dec2e7e29ef32bf5f1e7380b61d2b68bca38c
Mariana Ramos <7marianaramos@gmail.com>
2021-03-12 00:07:04 +0000

    tags na homepage

--- html/templates/newEditPostTemplate.php
+++ html/templates/newEditPostTemplate.php
@@ -40,33 +40,26 @@
                                 <option selected>Select the topic here</option>
                                 <option value="1">Water</option>
                                 <option value="2">Climate Change</option>
-                                <option value="3">Polution</option>
+                                <option value="3">Pollution</option>
                                 <option value="4">Biodiversity</option>
-                                <option value="5">Ecosystem</option>
+                                <option value="5">Ecossystem</option>
                                 <option value="6">Human Rights</option>
-                                <option value="7">Womens Empowerment</option>
+                                <option value="7">Women's Empowerment</option>
                                 <option value="8">Transports</option>
                             </select>
                         </div>
                     </div>
-                    <div class="row mt-3"> 
+                    <div class="row my-4 mx-1"> 
                         <form method="post">
                             <textarea id="mytextarea">Hello, World!</textarea>
                         </form>
-                    </div>
-                    <div class="form-group images-section">
-                        <label class="form-label" for="customFile">Default file input example</label>
-                        <input type="file" class="form-control" id="customFile" />
-                        <input type="file" class="form-control" id="customFile" />
-                        <input type="file" class="form-control" id="customFile" />
-                        <input type="file" class="form-control" id="customFile" />
                     </div>
                     <div class="form-group source-section">
                         <label for="inputNewsSource">News Source</label>
                         <input type="source" class="form-control" id="inputNewsSource" placeholder="Enter news source">
                         <small id="sourceHelp" class="form-text text-muted">It has to be a valid source, otherwise the post may be deleted</small>
                     </div>
-                    <button type="submit" class="btn-primary" formaction="homepage.php">Publish</button>
+                    <button type="submit" class="btn btn-primary" formaction="../pages/homepage.php">Publish</button>
                 </form>
             </div>
             <div class="col-md-1 col-xs-0"></div>
--- html/templates/post.php
+++ html/templates/post.php
@@ -169,8 +169,7 @@
             <div class="row post-interactions justify-content-between ">
                 <div class="col-xs-4 col-sm-4 col-md-2 actions mt-2 text-secondary pb-3"></i>
                     <div class="row ">
-                        <div class="col-md-6 col-xs-6 share action"><i class="fas fa-share-alt"></i></div>
-                        <div class="col-md-6 col-xs-6 save action"><span class="fas fa-edit"></div>
+                        <div class="col-md-6 col-xs-6 save action"><a href="../pages/editPostPage.php" ><span class="fas fa-edit"></a></div>
 
                     </div>
                 </div>
@@ -195,65 +194,3 @@
 
 
 
-<?php function drawOwnP($postID)
-{
-
-?>
-
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-    <link rel="stylesheet" href="../style/bootstrap.css"">
-
-    <div class=" container-fluid" id="post">
-    <div class="row">
-
-        <div class="col-12 justify-content-end ">
-            <div class="row justify-content-between post-header">
-                <div class="col-xs-12 col-sm-8">
-                    <div class="row ">
-                        <div class=" col-sm-1" id="profile-pic"><img class="" src="../images/profilepic1.jpg" alt="template pic" width="40" height="40" style="border-radius: 50%;"></div>
-                        <div class="col-10 " id="title">Low-level jets create winds of change for turbines</div>
-                    </div>
-                </div>
-                <div class="col-sm-3 col-md-3 tag-holder ">
-                    <button class="btn btn-primary tags">Ambient</button>
-                </div>
-            </div>
-
-            <div class="row justify-content-between post-content ">
-                <div class="col-lg-8 col-md-9 col-sm-12 ">
-                    <p class="post-text ">As one of the leading sources of clean and renewable energy, global wind power capacity has increased more than fivefold over the past decade, leading to larger turbines and pushing wind technology to its limits.</p>
-                    <br>
-                    <a href="../pages/fullPostPage.php" class="read-more">Read More</a>
-                </div>
-                <div class="col-3 post-pic">
-                    <img class="" src="../images/news1.jpg" alt="template pic" width="170">
-                </div>
-            </div>
-            <div class="row post-interactions justify-content-between">
-                <div class="col-2 actions"></i>
-                    <div class="row ">
-                        <div class="col-6 share action"><i class="fas fa-share-alt"></i></div>
-                        <div class="col-6 save action"><span class="fas fa-edit"></div>
-
-                    </div>
-                </div>
-                <div class="col-2" style="margin-right:0rem; padding-right:0rem ;margin-top:0.5rem">
-                    <div class="row justify-content-end votes text-secondary">
-                        <div class="col-6 upvote"><i class="fas fa-arrow-up"></i> 10 </div>
-                        <div class="col-6 downvote"><i class="fas fa-arrow-down"></i> 3 </div>
-
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
-
-    </div>
-    </div>
-
-
-
-
-
-<?php } ?>
