0ff72b30c6bb29da281f3404f3eebdce2a215d92
Flávia Carvalho Gavinha Pereira Carvalhido <up201806857@fe.up.pt>
2021-03-13 01:57:21 +0000

    Add content to reported post

--- html/templates/fullPostAdmin.php
+++ html/templates/fullPostAdmin.php
@@ -27,7 +27,7 @@
                         <a class="fas fa-arrow-left" href="../pages/adminDashboard.php"></a>
                     </div>
                     <div class="col-md-5 col-10">
-                        <p class="text-end">22-03-2020 | 22:22h</p>
+                        <p class="text-end">02-03-2021 | 12:57h</p>
                     </div>
                     <!--Imagem do avatar-->
                     <!-- <div class="col-md-1 col-10"><img src="https://blog.unyleya.edu.br/wp-content/uploads/2017/12/saiba-como-a-educacao-ajuda-voce-a-ser-uma-pessoa-melhor.jpeg" class="rounded-circle avatar" alt=""></div>-->
@@ -36,28 +36,32 @@
 
                 <div class="row justify-content-left">
                     <div class="col-md-12 col-12">
-                        <h2 class="row justify-content-between post-title"> Low-level jets create winds of change for turbines </h2>
-                        <div class="row font-weight-bold mt-4 mb-4">As one of the leading sources of clean and renewable energy, global wind power capacity has increased more than fivefold over the past decade, leading to larger turbines and pushing wind technology to its limits. </div>
+                        <h2 class="row justify-content-between post-title"> The Earth is Flat. Change my mind. </h2>
+                        <div class="row font-weight-bold mt-4 mb-4">NASA's landing on the moon is a fake, and photos taken at so called space are fake. such that astronauts sometimes need to be in space, displayed in the studio in the world, sometimes suddenly disappearing in the international space station, sometimes displayed without gloves outside the station, etc.</div>
                     </div>
 
 
                     <div class="row justify-content-center">
-                        <p class="col-md-6 ps-0 text-justify col-12">These much larger turbines are operating in very different atmospheric layers than smaller turbines used 5-10 years ago," said Srinidhi Gadde, one of the authors of a paper in the Journal of Renewable and Sustainable Energy, from AIP Publishing, that examines the impacts of turbine height. "At these scales, local meteorology and extreme shear events, which frequently occur, can impact power production." <br>
-                            Low-level jets, which are maxima in wind velocity in the lower atmosphere, are one cause for concern with growing turbines. <br>
+                        <p class="col-md-6 ps-0 text-justify col-12">The evidence for a flat earth is derived from many different facets of science and philosophy. The simplest is by relying on ones own senses to discern the true nature of the world around us. The world looks flat, the bottoms of clouds are flat, the movement of the sun; these are all examples of your senses telling you that we do not live on a spherical heliocentric world. This is using what's called an empiricist approach, or an approach that relies on information from your senses. Alternatively, when using Descartes' method of Cartesian doubt to skeptically view the world around us, one quickly finds that the notion of a spherical world is the theory which has the burden of proof and not flat earth theory.
+
+                            Perhaps the best example of flat earth proof is the Bedford Level Experiment. In short, this was an experiment performed many times on a six-mile stretch of water that proved the surface of the water to be flat. It did not conform to the curvature of the earth that round earth proponents teach.
+
+                            Many other experiments demonstrating the lack of curvature in the earth may be found in ''Earth Not a Globe'', by Samuel Rowbotham. <br>
                         </p>
-                        <div class="col-md-6 align-self-center col-12"><img src="../images/news1.jpg" class="d-block w-100" alt="..."></img> </div>
+                        <div class="col-md-6 align-self-center col-12"><img src="../images/flat_earth.jpg" class="d-block w-100" alt="..."></img> </div>
                     </div>
 
                     <p class="row">
-                    <div class="col-md-12 ps-0 text-justify col-12"> These strong, energetic wind flows can either have desirable or detrimental effects on the turbines, depending on how high the wind flows are in relation to the turbines. "A simple way to think about LLJs is to visualize them as high-velocity 'rivers' or 'streams' of wind within the atmosphere," Gadde said.<br>In their simulation of a wind farm with a 4-by-10 grid of turbines, Gadde and co-author Richard Stevens considered three different scenarios in which the LLJs were above, below, and in the middle of the turbine rotors. <br>
-                        When the jets and the turbines were at the same height, the researchers found the front rows blocked wind access downstream, causing a reduction in power production in each successive row. Relative to this equal height scenario, a larger downstream energy capture was observed in both other cases, though by different mechanisms. <br>
+                    <div class="col-md-12 ps-0 text-justify col-12"> The most commonly accepted explanation of this is that the space agencies of the world are involved in a conspiracy faking space travel and exploration. This likely began during the Cold War's 'Space Race', in which the U.S.S.R and USA were obsessed with beating each other into space to the point that each faked their accomplishments in an attempt to keep pace with the others supposed achievements. However, since the end of the Cold War, the conspiracy is most likely motivated by greed rather than political gains. Thus opening up a tremendous amounts of funds to embezzle as it only takes a fraction of the total budget to fake space travel.
+
+                        In light of the above, please note that we are not suggesting that space agencies are aware that the earth is flat and actively covering the fact up. They depict the earth as being round simply because that is what they expect it to be. <br>
                     </div>
                     </p>
 
                     <p class="row">
-                    <div class="col-12 ps-0 text-justify"> For high jets, the turbulence generated in the wakes of the turbines pulls the wind from the upper atmosphere down toward the turbines in a process called downward vertical kinetic energy entrainment, leading to large amounts of power production. More surprisingly, when the jets are low, the reverse process occurs. High-velocity wind from the LLJ is pushed upward into the turbine, a previously unknown phenomenon, which the authors termed upward vertical kinetic energy entrainment. <br>
-                        Gadde said he looks forward to applying this work to drive innovation and functionality to meet future power demands, which will require an even deeper understanding of events like LLJs and additional observations of these phenomena.
-                        "As one of the leading renewable energy technologies, wind energy is expected to deliver major contributions to the expected growth in renewable energy production in the coming decades," he said.
+                    <div class="col-12 ps-0 text-justify"> In general, we at the Flat Earth Society do not lend much credibility to photographic evidence. It is too easily manipulated and altered. Many of the videos posted here to "prove a round earth" by showing curvature will show no curvature or even convex curvature at parts. The sources are so inaccurate it's difficult to build an argument on them in either case. Furthermore, barrel distortion and other quirks of modern cameras will cause a picture to distort with little or no apparent altercation; especially without references within the picture. Photographs are also prone to distortion when taken through the bent glass of a pressurized cabin as well as atmospheric conditions on the outside. With this litany of problems, it's easy to see why photographic evidence is not to be trusted.
+
+                        It's also worth noting that if a person were to look down at the earth from high above, they would expect to see a circular shape where the sun's spotlight was shining. This explains why high altitude photographs are generally curved to produce the illusion of a round horizon.
                     </div>
                     </p>
 
@@ -65,7 +69,7 @@
 
 
                     <h6 class="row"> Story Source: </h6>
-                    <div class="row post-font"> Materials provided by American Institute of Physics. Note: Content may be edited for style and length.</div>
+                    <div class="row post-font"> https://www.theflatearthsociety.org/forum/index.php?topic=1324.0</div>
 
 
                     <div class="row  post-comment">
@@ -78,7 +82,7 @@
                                 <div class="post-comments">
                                     <p class="meta">Dec 18, 2014 <a href="#" class="commentlink">JohnDoe</a> says : <i class="pull-right"><a href="#" class="commentlink"><small>Reply</small></a> </i></p>
                                     <p>
-                                        So interesting! Wow I love it!!!
+                                        No way! This is FAKE News!
                                     </p>
                                 </div>
                             </li>
@@ -87,7 +91,7 @@
                                 <div class="post-comments">
                                     <p class="meta">Dec 19, 2014 <a href="#" class="commentlink">JohnDoe</a> says : <i class="pull-right"><a href="#" class="commentlink"><small>Reply</small></a></i></p>
                                     <p>
-                                        I had no idea about this! Who else is with me?
+                                        Is this true?
                                     </p>
                                 </div>
                                 <ul class="comments">
@@ -96,7 +100,7 @@
                                         <div class="post-comments">
                                             <p class="meta">Dec 20, 2014 <a href="#" class="commentlink">JohnDoe</a> says : <i class="pull-right"></i></p>
                                             <p>
-                                                Me me me!!
+                                                Read a book!
                                             </p>
                                         </div>
                                     </li>
