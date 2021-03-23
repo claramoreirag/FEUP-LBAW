67ccb4d6138d8a0fe1bc6840316575e265827663
Mariana Ramos <7marianaramos@gmail.com>
2021-03-23 16:47:32 +0000

    sql update

--- html/database/database.sql
+++ html/database/database.sql
@@ -1,32 +1,22 @@
 
-
-CREATE TYPE vote_type AS ENUM ('Up', 'Down');
 CREATE TYPE user_state AS ENUM ENUM ('Banned', 'Suspended', 'Active');
 CREATE TYPE report_state AS ENUM ('Accepted', 'Deleted', 'SuspendedUser', 'BanedUser');
-CREATE TYPE notification_state AS ENUM ('NotRead', 'Read');
 
 
 CREATE TABLE user(
-    id INTEGER PRIMARY KEY,
+    id INTEGER REFERENCES user(id) PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL
-);
-
-CREATE TABLE authenticated_user(
-    id INTEGER REFERENCES user(id) PRIMARY KEY,
     name TEXT NOT NULL,
     email UNIQUE NOT NULL,
     photo TEXT,
-    state user_state NOT NULL CHECK
-);
-
-CREATE TABLE admin_user(
-    id INTEGER REFERENCES user(id) PRIMARY KEY
+    state user_state NOT NULL CHECK, 
+    is_admin BOOLEAN NOT NULL
 );
 
 CREATE TABLE post(
     id INTEGER PRIMARY KEY,
-    "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
+    "datetime" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
     user INTEGER REFERENCES user(id) NOT NULL,
     body TEXT NOT NULL,
     category INTEGER REFERENCES category(id) NOT NULL
@@ -35,64 +25,33 @@ CREATE TABLE post(
 CREATE TABLE post_vote (
     id_user INTEGER REFERENCES user (id),
     id_post INTEGER REFERENCES post (id),
-    state vote_type NOT NULL
+    is_up BOOLEAN NOT NULL,
 );
 
 CREATE TABLE comment (
     id INTEGER PRIMARY KEY,
-    id_user INTEGER REFERENCES user (id),
-    id_post INTEGER REFERENCES post (id),
+    user_id INTEGER REFERENCES user (id),
+    post_id INTEGER REFERENCES post (id),
     body text,
-    date DateTime,
+    "datetime" DateTime,
+    comment_id REFERENCES comment(id)
 );
 
 CREATE TABLE report (
     id INTEGER PRIMARY KEY,
     id_user INTEGER REFERENCES user (id),
     date DateTime,
-    state report_state NOT NULL
-);
-
-CREATE TABLE comment report (
-    id INTEGER REFERENCES report (id),
-    id_comment INTEGER REFERENCES comment (id),
-);
-
-CREATE TABLE notification_follow(
-    id INTEGER PRIMARY KEY REFERENCES notification(id) ON UPDATE CASCADE,
-    follow_id INTEGER NOT NULL REFERENCES follow
-);
-
-CREATE TABLE notification_comment(
-    id INTEGER PRIMARY KEY REFERENCES notification(id) ON UPDATE CASCADE,
-    comment_id INTEGER NOT NULL REFERENCES comment
-);
-
-CREATE TABLE notification_vote(
-    id INTEGER PRIMARY KEY REFERENCES notification(id) ON UPDATE CASCADE,
-    vote_id INTEGER NOT NULL REFERENCES post_report
+    state report_state NOT NULL,
+    comment_id REFERENCES comment(id),
+    post_id REFERENCES post(id),
+    admin_id REFERENCES user(id)
 );
 
 CREATE TABLE follow(
-    id SERIAL PRIMARY KEY,
     follower INTEGER NOT NULL REFERENCES authenticated_user(id),
     followed INTEGER NOT NULL REFERENCES authenticated_user(id)
 );
 
-CREATE TABLE reply(
-    reply INTEGER PRIMARY KEY REFERENCES comment(id) ON UPDATE CASCADE,
-    comment INTEGER REFERENCES comment(id)
-);
-
-CREATE TABLE handle_report(
-    report_id INTEGER PRIMARY KEY REFERENCES report(id) ON UPDATE CASCADE
-    admin_id INTEGER REFERENCES admin_user(id)
-);
-
-CREATE TABLE post_report(
-  id INTEGER PRIMARY KEY REFERENCES report (id) ON UPDATE CASCADE,
-  id INTEGER REFERENCES post (id)
-);
 
 CREATE TABLE category(
   id SERIAL PRIMARY KEY,
@@ -104,10 +63,13 @@ CREATE TABLE reference(
   name text NOT NULL UNIQUE
 );
 
-CREATE TABLE notification(
+CREATE TABLE "notification"(
   id SERIAL PRIMARY KEY,
   "date" TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
   TYPE notification_state NOT NULL,
-  receiver INTEGER REFERENCES authenticated_user (id)
+  receiver INTEGER REFERENCES authenticated_user (id),
+  vote_id REFERENCES post_vote(post_id), /*??*/
+  comment_id REFERENCES comment(id),
+  follower_id REFERENCES user(id)
 );
 
