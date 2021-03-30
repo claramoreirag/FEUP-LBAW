1797338708a11bc6b309e508224244abd30e7c5a
Flávia Carvalho Gavinha Pereira Carvalhido <up201806857@fe.up.pt>
2021-03-30 14:17:36 +0100

    Finish queries

--- html/database/queries.sql
+++ html/database/queries.sql
@@ -1,77 +1,82 @@
 -- adminDashboard
 
--- Get all report's info (not answered reports)
+-- Get all report's info*
+SELECT report.id, report.date, user.name, report.comment_id, report.post_id
+FROM report, user
+WHERE report.user_id = user.id;
+
+-- Get all report's info (not answered reports)*
 SELECT report.id, report.date, user.name, report.comment_id, report.post_id
 FROM report, user
 WHERE report.state="NotAnswered" AND report.user_id = user.id; 
 
--- get all post reports
+-- get all post reports*
 SELECT report.id, report.state, report.date, user.name, report.post_id, report.admin_id
 FROM report, user
 WHERE report.user_id = user.id AND report.comment_id=NULL;
 
--- get all post reports (unread)
+-- get all post reports (unread)*
 SELECT report.id, report.date, user.name, report.post_id, report.admin_id
 FROM report, user
 WHERE report.state="NotAnswered" AND report.user_id = user.id AND report.comment_id=NULL;
 
---get comment reports
+--get comment reports*
 SELECT report.id, report.state, report.date, user.name, report.comment_id, report.admin_id
 FROM report, user
 WHERE report.user_id = user.id AND report.post_id=NULL;
 
---get comment reports (unread)
+--get comment reports (unread)*
 SELECT report.id, report.state, report.date, user.name, report.comment_id, report.admin_id
 FROM report, user
 WHERE report.state="NotAnswered" AND report.user_id = user.id AND report.post_id=NULL;
 
---get number of reports (post)
+--get number of reports (post)*
 SELECT COUNT(*)
 FROM report
 GROUP BY report.post_id;
 
---get number of reports (comment)
+--get number of reports (comment)*
 SELECT COUNT(*)
 FROM report
 GROUP BY report.comment_id;
 
---get user by ID (util)
+--get user by ID (util)*
 SELECT *
 FROM user
 WHERE user.id=$userID;
 
---get post by ID (util)
+--get post by ID (util)*
 SELECT *
 FROM post
 WHERE post.id=$postID;
 
 --homepage
 --authUserFeed
---get unread notifications
+--get unread notifications*
 SELECT "notification".id, "notification"."date", "notification".vote_id, "notification".comment_id, "notification".follower_id
 FROM "notification"
 WHERE "notification".receiver=$userid AND "notification".is_read = false
 ORDER BY DESC "notification"."date";
 
---get all notifications
+--get all notifications*
 SELECT "notification".id, "notification"."date", "notification".vote_id, "notification".comment_id, "notification".follower_id
 FROM "notification"
 WHERE "notification".receiver=$userid
 ORDER BY DESC "notification"."date";
 
---get vote notifications
+--get vote notifications*
 SELECT "notification".id, "notification"."date", "notification".vote_id
 FROM "notification"
 WHERE "notification".receiver=$userid AND "notification".follower_id = NULL AND "notification".comment_id = NULL
 ORDER BY DESC "notification"."date";
 
---get follow notifications
+--get follow notifications*
 SELECT "notification".id, "notification"."date", "notification".follower_id
 FROM "notification"
 WHERE "notification".receiver=$userid AND "notification".vote_id = NULL AND "notification".comment_id = NULL
 ORDER BY DESC "notification"."date";
 
---get comment notification
+--get comment notification*
 SELECT "notification".id, "notification"."date", "notification".comment_id
 FROM "notification"
 WHERE "notification".receiver=$userid AND "notification".vote_id = NULL AND "notification".follower_id = NULL
@@ -79,51 +84,51 @@ ORDER BY DESC "notification"."date";
 
 
 
---Get posts from followed user's
-SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name
+--Get posts from followed user's*
+SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name, post.upvotes, post.downvotes
 FROM post, user, category, follow
 WHERE follow.follower=$userid AND follow.followed=post.user_id AND post.category=category.id AND post.user_id=user.id
 ORDER BY DESC post.datetime;
 
---get posts from followed categories
-SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name
+--get posts from followed categories*
+SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name, post.upvotes, post.downvotes
 FROM post, user, category, follow_category
 WHERE follow_category.user_id=$userid AND follow_category.category_id=post.category AND post.category=category.id AND post.user_id=user.id
 ORDER BY DESC post.datetime;
 
---get more recent posts
-SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name
+--get more recent posts*
+SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name, post.upvotes, post.downvotes
 FROM post, user, category
 WHERE post.category=category.id AND post.user_id=user.id
 ORDER BY DESC post.datetime;
 
---get posts from category
-SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name
+--get posts from category*
+SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name, post.upvotes, post.downvotes
 FROM post, user, category
 WHERE post.category=category.id AND category.id=$selectedCategoryID AND post.user_id=user.id
 ORDER BY DESC post.datetime;
 
 
 --editPostPage
---get info to be edited 
+--get info to be edited* 
 SELECT post.id, post.body, post.header, post.title, category.name
 FROM post, user, category
-WHERE post.id=$editedPostID AND post.category=category.id AND post.user_id=$ownerID
-ORDER BY DESC post.datetime;
+WHERE post.id=$editedPostID AND post.category=category.id AND post.user_id=$userID;
+
 
 --fullPostPageAdmin
 --fullPostPage
---get full post
-SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name
+--get full post*
+SELECT post.id, post."datetime", user.name, post.body, post.header, post.title, category.name, post.upvotes, post.downvotes
 FROM post, user, category
 WHERE post.category=category.id AND post.id=$selectedPostID AND post.user_id=user.id;
 
---get post references
+--get post references*
 SELECT reference.name
 FROM reference, post_reference
 WHERE reference.id = post_reference.reference_id AND post_reference.post_id=$selectedPostID;
 
---get comments from post
+--get comments from post*
 SELECT comment.id, comment.date, user.name, comment.body
 FROM post, user, comment
 WHERE comment.post_id=$selectedPostID AND comment.user_id=user.id
@@ -131,73 +136,76 @@ ORDER BY DESC comment.date;
 
 
 --login
---get Auth user
+--get Auth user*
 SELECT *
 FROM user
 WHERE user.username=$username AND user.password=$password AND user.state="Active";
 
 
 --myProfilePage
---get personal info
+--get personal info*
 SELECT user.username, user.name, user.photo
 FROM user
 WHERE user.id=$userID;
 
---get Own Posts
-SELECT post.id, post."datetime", post.header, post.title, user.name, category.name
+--get Own Posts*
+SELECT post.id, post."datetime", post.header, post.title, user.name, category.name, post.upvotes, post.downvotes
 FROM post, user, category
 WHERE post.category=category.id AND post.user_id=$userID
 ORDER BY DESC post.datetime;
 
---get Saved
-SELECT post.id, post."datetime", post.header, post.title, user.name, category.name
+--get Saved*
+SELECT post.id, post."datetime", post.header, post.title, user.name, category.name, post.upvotes, post.downvotes
 FROM post, user, category, saved_post
 WHERE saved_post.post_id = post.id AND post.category=category.id AND saved_post.user_id = $userID
 ORDER BY DESC post.datetime;
 
---get Upvotes
-SELECT post.id, post."datetime", post.header, post.title, user.name, category.name
+--get Upvotes*
+SELECT post.id, post."datetime", post.header, post.title, user.name, category.name, post.upvotes, post.downvotes
 FROM post, user, category, post_vote
 WHERE post.category=category.id AND post.user_id=user.id AND post_vote.post_id=post.id AND post_vote.user_id=$userID AND post_vote.is_up=true
 ORDER BY DESC post.datetime;
 
 
 --otherProfile
---getOthersInfo
+--getOthersInfo*
 SELECT user.username, user.name, user.photo
 FROM user
 WHERE user.id=$otherUserID;
 
---get Others Posts
-SELECT post.id, post."datetime", post.header, post.title, user.name, category.name
+--get Others Posts*
+SELECT post.id, post."datetime", post.header, post.title, user.name, category.name, post.upvotes, post.downvotes
 FROM post, user, category
 WHERE post.category=category.id AND post.user_id=$otherUserID
 ORDER BY DESC post.datetime;
 
---get Others Upvotes
-SELECT post.id, post."datetime", post.header, post.title, user.name, category.name
+--get Others Upvotes*
+SELECT post.id, post."datetime", post.header, post.title, user.name, category.name, post.upvotes, post.downvotes
 FROM post, user, post_vote
 WHERE post.category=category.id AND post.user_id=user.id AND post_vote.post_id=post.id AND post_vote.user_id=$otherUserID AND post_vote.is_up=true
 ORDER BY DESC post.datetime;
 
 
 --userManager
---get All users
+--get All users*
 SELECT user.username, user.name, user.photo
 FROM user;
 
---get all suspended users
+--get all suspended users*
 SELECT user.username, user.name, user.photo
 FROM user
 WHERE user.user_state="Suspended";
 
---get all banned users
+--get all banned users*
 SELECT user.username, user.name, user.photo
 FROM user
 WHERE user.user_state="Banned";
 
---get all active users
+--get all active users*
 SELECT user.username, user.name, user.photo
 FROM user
 WHERE user.user_state="Active";
+
+/* TODO: get number of followers, following, upvotes for current user and other users */
+
 
